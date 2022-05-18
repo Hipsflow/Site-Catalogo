733c30665ec88c2bcad9fde5fa15c18cf1518460
#Kaliocean8 <caua12.pisoni@gmail.com>
2022-05-18 16:37:37 -0300

    Algumas coisas refatoradas, scroll to top, adicionado

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,5 @@
 
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
     const showNavbar = (toggleId, navId, bodyId, headerId) => {
@@ -36,8 +37,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
     linkColor.forEach(l => l.addEventListener('click', colorLink))
 
     $('.multiple-items').slick({
-        infinite: true,
+        dots: true,
+        infinite: false,
         slidesToShow: 4,
-        slidesToScroll: 4
-      });
+        slidesToScroll: 5,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 5,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 5
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+
+});
+
+var btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0,0);
 });
